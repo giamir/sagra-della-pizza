6dbd32c71be66dcb2651bb42126e8aee64c17c77
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2026-06-19 16:53:28 +0200

    Fix mac app icon packaging

--- apps/desktop/electron-builder.config.js
+++ apps/desktop/electron-builder.config.js
@@ -12,21 +12,21 @@ const config = {
   extraResources: [{ from: 'resources/', to: '.' }],
   win: {
     target: [{ target: 'nsis', arch: ['x64'] }],
-    icon: 'build/icon.png'
+    icon: 'resources/logo.png'
   },
   nsis: {
     oneClick: false,
     allowToChangeInstallationDirectory: true,
     perMachine: false,
-    installerIcon: 'build/icon.png',
-    uninstallerIcon: 'build/icon.png'
+    installerIcon: 'resources/logo.png',
+    uninstallerIcon: 'resources/logo.png'
   },
   mac: {
     target: [
       { target: 'dmg', arch: ['x64', 'arm64'] }
     ],
     category: 'public.app-category.business',
-    icon: 'build/icon.png'
+    icon: 'resources/icon.icns'
   }
 }
 
